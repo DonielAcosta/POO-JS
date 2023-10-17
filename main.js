be5ec3c545ae3db94ad6fa2a92b3036749786f43
Doniel Acosta <donielacosta1995@gmail.com>
2023-10-16 21:09:35 -0400

    .

---
+++ main.js
@@ -0,0 +1,13 @@
+const doniel ={
+    name:"Doniel",
+    age:27,
+    cursosAprobados:[
+        "curso PHP",
+        "curso PYTHON",
+        "curso RUBY",
+        "curso JAVASCRIP"
+    ]
+};
+
+// DONIEL APROBO EL CURSO
+doniel.cursosAprobadosgit
