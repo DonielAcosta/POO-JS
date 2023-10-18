41a3d0cd2508c2b2e884d9f9319c249e1227dc46
Doniel Acosta <donielacosta1995@gmail.com>
2023-10-17 22:08:45 -0400

    objet literal

--- main.js
+++ main.js
@@ -6,8 +6,55 @@ const doniel ={
         "curso PYTHON",
         "curso RUBY",
         "curso JAVASCRIP"
-    ]
+    ],
+
+    aprobarCurso(newCurso){
+        this.cursosAprobados.push(newCurso);
+  
+    }
 };
 
 // DONIEL APROBO EL CURSO
-doniel.cursosAprobadosgit
+doniel.cursosAprobados.push("Laravel");
+
+function Student(name,age,cursosAprobados){
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+}
+Student.prototype.aprobarCurso = function(newCurso){
+    this.cursosAprobados.push(newCurso);
+}
+
+const duby = new Student(
+
+    "Duby Acosta",
+    21,
+    [
+        "Diseno",
+        "html"
+    ]
+);
+
+// prototipos con sintaxis de  clas
+class Studen2{
+    constructor({name,age,cursosAprobados}){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    aprobarCurso(newCurso){
+        this.cursosAprobados.push(newCurso);
+
+    }
+}
+
+const abc = new Studen2(
+
+  { name: "Duby solanger",
+    age:21,
+    cursosAprobados:[
+        "Diseno 2",
+    ],}
+);
