22999263d34ca39f58586a402ef197f12739f53f
doniel <donielacosta1995@gmail.com>
2023-10-21 15:28:59 -0400

    cambios

--- main.js
+++ main.js
@@ -1,60 +1,82 @@
-const doniel ={
-    name:"Doniel",
-    age:27,
-    cursosAprobados:[
-        "curso PHP",
-        "curso PYTHON",
-        "curso RUBY",
-        "curso JAVASCRIP"
-    ],
+// const doniel ={
+//     name:"Doniel",
+//     age:27,
+//     cursosAprobados:[
+//         "curso PHP",
+//         "curso PYTHON",
+//         "curso RUBY",
+//         "curso JAVASCRIP"
+//     ],
 
-    aprobarCurso(newCurso){
-        this.cursosAprobados.push(newCurso);
+//     aprobarCurso(newCurso){
+//         this.cursosAprobados.push(newCurso);
   
-    }
-};
+//     }
+// };
 
-// DONIEL APROBO EL CURSO
-doniel.cursosAprobados.push("Laravel");
+// // DONIEL APROBO EL CURSO
+// doniel.cursosAprobados.push("Laravel");
 
-function Student(name,age,cursosAprobados){
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-}
-Student.prototype.aprobarCurso = function(newCurso){
-    this.cursosAprobados.push(newCurso);
-}
+// function Student(name,age,cursosAprobados){
+//     this.name = name;
+//     this.age = age;
+//     this.cursosAprobados = cursosAprobados;
+// }
+// Student.prototype.aprobarCurso = function(newCurso){
+//     this.cursosAprobados.push(newCurso);
+// }
 
-const duby = new Student(
+// const duby = new Student(
 
-    "Duby Acosta",
-    21,
-    [
-        "Diseno",
-        "html"
+//     "Duby Acosta",
+//     21,
+//     [
+//         "Diseno",
+//         "html"
+//     ]
+// );
+
+// // prototipos con sintaxis de  clas
+// class Studen2{
+//     constructor({name,age,cursosAprobados}){
+//         this.name = name;
+//         this.age = age;
+//         this.cursosAprobados = cursosAprobados;
+//     }
+
+//     aprobarCurso(newCurso){
+//         this.cursosAprobados.push(newCurso);
+
+//     }
+// }
+
+// const abc = new Studen2(
+
+//   { name: "Duby solanger",
+//     age:21,
+//     cursosAprobados:[
+//         "Diseno 2",
+//     ],}
+// );
+
+
+const doni ={
+    name: "Doni1995",
+    username:"doni1995",
+    points:100,
+    redes:{
+        twitter:"Doniel_acosta",
+        instagram:"donielacosta",
+        facebook: undefined
+    },
+    approvedCourses:[
+        "javascript",
+        "curso basico CSS"
+    ],
+    learningPaths:[
+        {
+            name:"Escuela de Desarrollo WEB"
+        },
+        {},
     ]
-);
-
-// prototipos con sintaxis de  clas
-class Studen2{
-    constructor({name,age,cursosAprobados}){
-        this.name = name;
-        this.age = age;
-        this.cursosAprobados = cursosAprobados;
-    }
-
-    aprobarCurso(newCurso){
-        this.cursosAprobados.push(newCurso);
-
-    }
 }
-
-const abc = new Studen2(
-
-  { name: "Duby solanger",
-    age:21,
-    cursosAprobados:[
-        "Diseno 2",
-    ],}
-);
