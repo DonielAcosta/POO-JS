a9fe60b470e51ce7ded78d4fb3959951e66fec73
Doniel Acosta <donielacosta1995@gmail.com>
2023-11-15 22:08:58 -0400

    abstraccion

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
+class Course {
+    constructor({
+      name,
+      classes = [],
+    }) {
+      this.name = name;
+      this.classes = classes;
+    }
+  }
+  
+  const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programación Básica",
+  });
+  const cursoDefinitivoHTML = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+  });
+  const cursoPracticoHTML = new Course({
+    name: "Curso Practico de HTML y CSS",
+  });
+  
 class LearningPath{
-
+    constructor({
+        name, 
+        courses =[]
+    }){
+        this.name =name;
+        this.courses =courses;
+    }
 }
-const escuelaWeb = new LearningPath();
-const escuelaData = new LearningPath();
-const escuelaDesign = new LearningPath();
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+      cursoProgBasica,
+      cursoDefinitivoHTML,
+      cursoPracticoHTML,
+    ],
+  });
+  
+  const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+      cursoProgBasica,
+      "Curso DataBusiness",
+      "Curso Dataviz",
+    ],
+  });
+  
+  const escuelaVgs = new LearningPath({
+    name: "Escuela de Vidweojuegos",
+    courses: [
+      cursoProgBasica,
+      "Curso de Unity",
+      "Curso de Unreal",
+    ],
+  })
+  
 
 class Student{
     constructor({
@@ -34,13 +84,19 @@ const doniel = new Student({
     username:"doni1995",
     email:"donielacosta1995@gmail.com",
     twitter: "donielacosta",
-    learningPaths
+    learningPaths: [
+        escuelaWeb,
+        escuelaVgs,
+      ],
 });
 
 const dubexy = new Student({
     name:"dubexy",
     username:"duby01",
     email:"duby01@gmail.com",
-    instagram: "duby"
-
+    instagram: "duby",
+    learningPaths: [
+        escuelaWeb,
+        escuelaData,
+      ],
 });
