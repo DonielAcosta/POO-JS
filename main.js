baf04fc8f7a9748ad91f0e5139464b658e110a7e
doniel <donielacosta1995@gmail.com>
2023-11-17 20:43:51 -0400

    ruta de aprendizaej

--- main.js
+++ main.js
@@ -426,6 +426,78 @@ jose.name = "Jose Alberto";
 //Factory pattern y RORO
 /****************** */
 
+function isObject(subject) {
+  return typeof subject == "object";
+}
+
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+  let copySubject;
+
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
+
+
+function requiredParam(param) {
+  throw new Error(param + " es obligatorio");
+}
+
+function createLearningPath({
+  name = requiredParam("name"),
+  courses = [],
+}) {
+  const private = {
+    "_name": name,
+    "_courses": courses,
+  };
+
+  const public = {
+    get name() {
+      return private["_name"];
+    },
+    set name(newName) {
+      if (newName.length != 0) {
+        private["_name"] = newName;
+      } else {
+        console.warn("Tu nombre debe tener al menos 1 caracter");
+      }
+    },
+    get courses() {
+      return private["_courses"];
+    },
+  };
+
+  return public;
+}
+
 function createStudent({
   name = requiredParam("name"),
   email = requiredParam("email"),
@@ -438,44 +510,50 @@ function createStudent({
 } = {}) {
   const private = {
     "_name": name,
+    "_learningPaths": learningPaths,
   };
 
   const public = {
     email,
     age,
     approvedCourses,
-    learningPaths,
     socialMedia: {
       twitter,
       instagram,
       facebook,
     },
-    get name(){
-     return  private["_name"];
+    get name() {
+      return private["_name"];
     },
-    set name(newName){
-      if (newName.length !=0) {
+    set name(newName) {
+      if (newName.length != 0) {
         private["_name"] = newName;
-      }else{
-        console.warn("tu nombre debe tener al menos 1 caracter");
+      } else {
+        console.warn("Tu nombre debe tener al menos 1 caracter");
       }
-    }
-    // readName() {
-    //   return private["_name"];
-    // },
-    // changeName(newName) {
-    //   private["_name"] = newName;
-    // },
-  };
+    },
+    get learningPaths() {
+      return private["_learningPaths"];
+    },
+    set learningPaths(newLP) {
+      if (!newLP.name) {
+        console.warn("Tu LP no tiene la propiedad name");
+        return;
+      }
 
-  Object.defineProperty(public, "readName", {
-    writable: false,
-    configurable: false,
-  });
-  Object.defineProperty(public, "changeName", {
-    writable: false,
-    configurable: false,
-  });
+      if (!newLP.courses) {
+        console.warn("Tu LP no tiene courses");
+        return;
+      }
+
+      if (!isArray(newLP.courses)) {
+        console.warn("Tu LP no es una lista (*de cursos)");
+        return;
+      }
+      
+      private["_learningPaths"].push(newLP);
+    },
+  };
 
   return public;
 }
